2b975771d0ba158f4bfaaafbcbaea41be39e0a5a
Bruno Rayan <77452930+BrunoRayan24@users.noreply.github.com>
2022-04-26 23:04:22 -0300

    Add files via upload

---
+++ Projeto BI/Loja.sql
@@ -0,0 +1,3 @@
+insert into Loja (cod_loja, estado, cidade, bairro) values ('loja_1', 'Rio de janeiro', 'Niteroi', 'Boa Viagem');
+insert into Loja (cod_loja, estado, cidade, bairro) values ('loja_2', 'Rio de janeiro', 'Duque de caxias', 'Jardim Gramacho');
+insert into Loja (cod_loja, estado, cidade, bairro) values ('loja_3', 'Rio de janeiro', 'Madureira', 'Intendente Magalhães');
---
+++ Projeto BI/funcionario(2).sql
@@ -0,0 +1,30 @@
+create table funcionario (
+	cod_funcionario INT,
+	nome_funcionario VARCHAR(50),
+	genero_funcionario VARCHAR(50)
+);
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (1, 'Neely Swanton', 'M');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (2, 'Dinny Pauleau', 'F');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (3, 'Annabelle Selwyne', 'F');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (4, 'Fern Luton', 'F');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (5, 'Alyssa Christophersen', 'M');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (6, 'Chloris Stallion', 'F');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (7, 'Keen Beere', 'M');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (8, 'Grissel Mathely', 'F');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (9, 'Ester Rolston', 'F');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (10, 'Jared Kingcote', 'M');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (11, 'Prescott D''Elias', 'M');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (12, 'Pepe Gethings', 'F');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (13, 'Sander Meneghelli', 'F');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (14, 'Elaine Cristofol', 'F');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (15, 'Alex Desport', 'M');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (16, 'Nelia Soars', 'M');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (17, 'Ced Boffey', 'F');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (18, 'Jenica Gunby', 'F');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (19, 'Demott Gavaran', 'F');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (20, 'Fletch O''Hallihane', 'F');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (21, 'Crystal Johncey', 'F');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (22, 'Rosemonde Skellon', 'F');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (23, 'Dru Loughhead', 'F');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (24, 'Brendon Leades', 'M');
+insert into funcionario (cod_funcionario, nome_funcionario, genero_funcionario) values (25, 'Duffie Gilchriest', 'F');
